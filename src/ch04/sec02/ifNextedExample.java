5689d76358913e901800ce74bb0bb9b61344632e
glory1223 <hilyk1223@gmail.com>
2024-02-13 10:45:15 +0900

    240213

---
+++ src/ch04/sec02/ifNextedExample.java
@@ -0,0 +1,26 @@
+package ch04.sec02;
+
+import java.sql.SQLOutput;
+
+public class ifNextedExample {
+    public static void main(String[] args) {
+        /*
+         * 랜덤한 숫자를 구하는 공식
+         * start부터 n개의 정수를 랜덤한 숫자로 구한다.
+         * (int) (Math.random()*n) + start;
+         */
+        int score =  (int) (Math.random()*20) + 81; // 81 ~100 까지의 랜덤한 숫자를 구한다.
+        System.out.println("랜덤한 점수: " + score);
+
+        String grade ;
+
+        if(score >= 90) {
+            if (score >= 95) grade = "A+";
+            else grade = "A";
+        } else {
+            if (score >=85) grade ="B+";
+            else grade = "B";
+        }
+        System.out.println("학점: "+ grade);
+    }
+}
